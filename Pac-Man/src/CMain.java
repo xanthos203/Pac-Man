8114e29b2bb9a253d9e85426019f2ac1aa817e65
Manuel Glantschnig <glantschnig.m@hotmail.com>
2016-02-01 20:28:58 +0100

    Second change.

--- Pac-Man/src/CMain.java
+++ Pac-Man/src/CMain.java
@@ -7,6 +7,5 @@ public class CMain
 	public static void main(String[] args) 
 	{
 		CLogDB logdb = new CLogDB("C:/Users/Thomas/Desktop/Pac-Man Projekt/Mappe1.csv");
-		CLogDB logdb1 = new CLogDB("C:/Users/Thomas/Desktop/Pac-Man Projekt/Mappe1.csv");
 	}
 }
