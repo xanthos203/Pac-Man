4319cf444b8a12cc6d03b063b5a2133910b592c8
Manuel Glantschnig <glantschnig.m@hotmail.com>
2016-02-01 20:45:11 +0100

    test

--- Pac-Man/src/CMain.java
+++ Pac-Man/src/CMain.java
@@ -4,8 +4,6 @@ import control.CLogDB;
 
 public class CMain
 {
-	// Test kommentar
-	
 	public static void main(String[] args) 
 	{
 		CLogDB logdb = new CLogDB("C:/Users/Thomas/Desktop/Pac-Man Projekt/Mappe1.csv");
