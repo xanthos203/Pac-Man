f19f5b76bee2aca03c501203ec7e1287065c2ac4
Manuel Glantschnig <glantschnig.m@hotmail.com>
2016-02-05 21:31:12 +0100

    Bilder mitincludiert

--- Pac-Man/src/control/CLogzeile.java
+++ Pac-Man/src/control/CLogzeile.java
@@ -2,9 +2,9 @@ package control;
 
 public class CLogzeile 
 {
-	public CLogzeile(String string,  String string2, String string3,
+	public CLogzeile(String string1, String string2, String string3,
 					 String string4, String string5, String string6)
 	{
-		System.out.printf("   "+string+ " "+string2+" "+string3+" "+string4+" "+string5+" "+string6);
+		System.out.printf("   " + string1 + " " + string2 + " " + string3 + " " + string4 + " " + string5 + " " + string6);
 	}
 }
