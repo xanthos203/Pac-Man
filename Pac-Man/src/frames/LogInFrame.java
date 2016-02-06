abb352f9126c065cd7c95f832407129ba8493327
Manuel Glantschnig <glantschnig.m@hotmail.com>
2016-02-07 00:46:06 +0100

    Fertiges LogIn-Fenster

---
+++ Pac-Man/src/frames/LogInFrame.java
@@ -0,0 +1,87 @@
+package frames;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public final class LogInFrame extends JFrame implements KeyListener
+{
+	private Color 		backgroundColor	= new Color(38, 0, 38);
+	private JPanel 		contentPane		= new JPanel();
+	private Icon		pacmanIcon		= new ImageIcon(Toolkit.getDefaultToolkit().getImage(LogInFrame.class.getResource("/images/Pac-Man.PNG"))),
+						infoIcon		= new ImageIcon(Toolkit.getDefaultToolkit().getImage(LogInFrame.class.getResource("/images/Info_i.PNG"))),
+						infoText		= new ImageIcon(Toolkit.getDefaultToolkit().getImage(LogInFrame.class.getResource("/images/Info_Text.PNG")));
+	private JPanel		infoPanel		= new JPanel(),
+						usernamePanel	= new JPanel();
+	private JLabel		text			= new JLabel(),
+						pmImage_label	= new JLabel(pacmanIcon),
+						infoImage_label	= new JLabel(infoIcon),
+						infoText_label	= new JLabel(infoText);
+	private JTextField	usernameFeld	= new JTextField();
+	private int 		screenWidth		= Toolkit.getDefaultToolkit().getScreenSize().width,  
+						screenHeight	= Toolkit.getDefaultToolkit().getScreenSize().height,
+						frameWidth		= 1100,
+						frameHeight		= 735;
+	private String		username		= null;
+		
+	public LogInFrame()
+	{
+		setIconImage(Toolkit.getDefaultToolkit().getImage(LogInFrame.class.getResource("/images/Pac-Man_icon.PNG")));
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("LogIn");
+		setVisible(true);
+		setResizable(false);
+		setSize(frameWidth, frameHeight);
+		setLocation(screenWidth / 2 - frameWidth / 2, screenHeight / 2 - frameHeight / 2);
+		add(contentPane);
+		contentPane.setBackground(backgroundColor);
+		contentPane.setLayout(new BorderLayout());
+		
+		infoPanel.setBackground(backgroundColor);
+		infoPanel.setLayout(new BorderLayout());
+		infoPanel.add(infoImage_label, BorderLayout.WEST);
+		infoPanel.add(infoText_label, BorderLayout.EAST);
+		
+		text.setText("Bitte Spielernamen eingeben:");
+		text.setFont(new Font("arial", Font.BOLD, 12));
+		text.setForeground(Color.WHITE);
+		
+		usernameFeld.setColumns(7);
+		usernameFeld.setFont(new Font("arial", Font.PLAIN, 27));
+		usernameFeld.setHorizontalAlignment(SwingConstants.LEFT);
+		usernameFeld.addKeyListener(this);
+		
+		usernamePanel.setLayout(new GridBagLayout());
+		GridBagConstraints center = new GridBagConstraints();
+		center.anchor = GridBagConstraints.CENTER;
+		center.fill = GridBagConstraints.NONE;
+		usernamePanel.add(usernameFeld, center);
+		usernamePanel.setBackground(backgroundColor);
+		
+		contentPane.add(pmImage_label, BorderLayout.WEST);
+		contentPane.add(usernamePanel, BorderLayout.CENTER);
+		contentPane.add(infoPanel, BorderLayout.EAST);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e)
+	{
+		if(e.getKeyCode() == KeyEvent.VK_ENTER)
+		{
+			if(usernameFeld.getText().equals("") || (usernameFeld.getText().equals(" ")))
+				JOptionPane.showMessageDialog(null, "Bitte geben Sie einen g\u00FCltigen Spielernamen ein!", "Fehler", JOptionPane.ERROR_MESSAGE);
+			else
+			{
+				username = usernameFeld.getText();
+				System.exit(0);
+				// Hauptfenster öffnen
+			}
+		}
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {}
+	@Override
+	public void keyReleased(KeyEvent e) {}
+}
--- Pac-Man/src/main/CMain.java
+++ Pac-Man/src/main/CMain.java
@@ -1,9 +1,13 @@
+package main;
+
 import control.CLogDB;
+import frames.LogInFrame;
 
 public class CMain
 {
 	public static void main(String[] args) 
 	{
+		LogInFrame loginframe = new LogInFrame();
 		CLogDB logdb = new CLogDB(System.getProperty("user.dir") + "\\src\\view\\GUI.csv");
 	}
 }
