7fa6b5a15cdb4d1973632f24c8aa0d0a0f4e5dd0
Manuel Glantschnig <glantschnig.m@hotmail.com>
2016-03-29 23:35:14 +0200

    Feinheiten

--- Pac-Man/src/view/frames/GameMainFrame.java
+++ Pac-Man/src/view/frames/GameMainFrame.java
@@ -2,7 +2,6 @@ package view.frames;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
